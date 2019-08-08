dff58c0179c2c0ae756ffa115bf72148867f4456
Rushikesh Gunjal <52359509+RushiG02@users.noreply.github.com>
2019-08-08 22:25:46 +0530

    Update correlation_regression3.py

--- correlation_regression3.py
+++ correlation_regression3.py
@@ -1,22 +1,22 @@
 import math
-Score1 = [15 , 12 , 8  , 8 ,  7 ,  7 ,  7 ,  6   , 5  , 3] 
-Score2 = [10  ,25 , 17  ,11 , 13 , 17 , 20 , 13 , 9  , 15]
-m1 = (sum(Score1)/len(Score1))
-m2 = (sum(Score2)/len(Score1))
+phy_marks = [15 , 12 , 8  , 8 ,  7 ,  7 ,  7 ,  6   , 5  , 3] 
+hist_marks = [10  ,25 , 17  ,11 , 13 , 17 , 20 , 13 , 9  , 15]
+m1 = (sum(phy_marks)/len(phy_marks))
+m2 = (sum(hist_marks)/len(hist_marks))
 
-Score_1 = [x-m1 for x in Score1]
-Score_2 = [x-m2 for x in Score2]
+Score_1 = [x-m1 for x in phy_marks]
+Score_2 = [x-m2 for x in hist_marks]
 
 n1 = sum([x*y for x,y in zip(Score_1,Score_2)])
 
-Score1 = sum([(x-m1)**2 for x in Score1])
-Score2 = sum([(x-m2)**2 for x in Score2])
+phy_marks = sum([(x-m1)**2 for x in phy_marks])
+hist_marks = sum([(x-m2)**2 for x in hist_marks])
 
-n2 = (Score1**(1/2))*(Score2**(1/2))
+n2 = (phy_marks**(1/2))*(hist_marks**(1/2))
 
 r = n1/n2
-sd1 = math.sqrt(Score1/len(Score_1))
-sd2 = math.sqrt(Score2/len(Score_2))
+sd1 = math.sqrt(phy_marks/len(Score_1))
+sd2 = math.sqrt(hist_marks/len(Score_2))
 b1 = r*sd2/sd1
 b0 = m2-b1*m1
 
